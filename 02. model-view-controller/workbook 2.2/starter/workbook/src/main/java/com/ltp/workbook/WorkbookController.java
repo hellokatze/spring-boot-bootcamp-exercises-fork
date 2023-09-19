dc1076beff98c04d243b344aa5bd51c10b05726e
Katharine Lin <katharine.exe@gmail.com>
2023-09-19 16:04:14 +0200

    refactor with ModelAndView

--- 02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,25 +1,25 @@
 package com.ltp.workbook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WorkbookController {
 
   @GetMapping("/shows")
-  public String getShows(Model model) {
-    Show show1 = new Show("Breaking Bad", "Ozymandias", 10.0);
-    Show show2 = new Show("Attack on Titan", "Hero", 9.9);
-    Show show3 = new Show("Attack on Titan", "Perfect Game", 9.9);
-    Show show4 = new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9);
-    Show show5 = new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9);
-    model.addAttribute("show1", show1);
-    model.addAttribute("show2", show2);
-    model.addAttribute("show3", show3);
-    model.addAttribute("show4", show4);
-    model.addAttribute("show5", show5);
-    return "shows";
+  public ModelAndView getShows() {
+    Map<String, Show> model = new HashMap<String, Show>();
+    model.put("first", new Show("Breaking Bad", "Ozymandias", 10.0));
+    model.put("second", new Show("Attack on Titan", "Hero", 9.9));
+    model.put("third", new Show("Attack on Titan", "Perfect Game", 9.9));
+    model.put("fourth", new Show("Star Wars: The Clone Wars", "Victory and Death", 	9.9));
+    model.put("fifth", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
+    return new ModelAndView("shows", model);
   }
 
 }
