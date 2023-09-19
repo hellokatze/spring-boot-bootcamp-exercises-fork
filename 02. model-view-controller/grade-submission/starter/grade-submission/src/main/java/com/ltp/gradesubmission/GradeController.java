dd5d998385bad6f9f400d84bad9615a24a7ca353
Katharine Lin <katharine.exe@gmail.com>
2023-09-19 10:55:39 +0200

    add Model

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,13 +1,14 @@
 package com.ltp.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
 
-  @GetMapping("/hello")
-  public String sayHello() {
-    return "someHTMLview";
+  @GetMapping("/grades")
+  public String getGrades(Model model) {
+    return "grades";
   }
 }
