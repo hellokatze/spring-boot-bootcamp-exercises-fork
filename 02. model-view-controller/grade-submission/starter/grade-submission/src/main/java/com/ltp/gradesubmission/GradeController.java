81f577c2f5d5e047647f6eb5ef4621b3c3584387
Katharine Lin <katharine.exe@gmail.com>
2023-09-21 11:00:09 +0200

    loop through studentGrades Array as List

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,16 +1,25 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+  // studentGrades list will be populated with data ONLY when GradeController is initialized.
+  List<Grade> studentGrades = Arrays.asList(
+    new Grade("Harry", "Potions", "C-"),
+    new Grade("Hermione", "Arithmency", "A+"),
+    new Grade("Neville", "Charms", "A-")
+  );
 
   @GetMapping("/grades")
   public String getGrades(Model model) {
-    Grade grade = new Grade("Harry", "Potions", "C-");
-    model.addAttribute("grade", grade); // stores data in Model attribute
+    model.addAttribute("grades", studentGrades); // stores data in Model attribute
     return "grades";
   }
 }
