6cdacdeff548a3fe407a6682825b25dbdee4ed50
Katharine Lin <katharine.exe@gmail.com>
2023-09-26 09:18:40 +0200

    Add update button to grades

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
@@ -22,13 +23,31 @@ public class GradeController {
 
   @PostMapping("/handleSubmit")
   public String submitGrade(Grade grade) { // BTS Spring Boot creates empty Grade object using empty constructor and setters to update field with payload from POST request.
-    studentGrades.add(grade);
+    int index = getGradeIndex(grade.getName());
+    if (index == -1000) {
+      studentGrades.add(grade);
+    } else {
+      studentGrades.set(index, grade);
+    }
     return "redirect:/grades";
   }
 
   @GetMapping("/")
-  public String gradeForm(Model model) {
-    model.addAttribute("grade", new Grade());
+  public String gradeForm(Model model, @RequestParam(required = false) String name) {
+    Grade grade;
+    if (getGradeIndex(name) == -1000) {
+      grade = new Grade();
+    } else {
+      grade = studentGrades.get(getGradeIndex(name));
+    }
+    model.addAttribute("grade", getGradeIndex(name)   == -1000 ? new Grade() : grade);
     return "form";
+  }
+
+  public Integer getGradeIndex(String name) {
+    for (int i = 0; i < studentGrades.size(); i++) {
+      if (studentGrades.get(i).getName().equals(name)) return i;
+    }
+    return -1000;
   }
 }
