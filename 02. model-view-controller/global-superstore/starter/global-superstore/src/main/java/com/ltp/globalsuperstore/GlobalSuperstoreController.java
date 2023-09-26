0ab694d0ee6d58ffa9a90ebcbdc3fa774d5ea691
Katharine Lin <katharine.exe@gmail.com>
2023-09-26 12:30:48 +0200

    add flash attribute

--- 02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
+++ 02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class GlobalSuperstoreController {
@@ -32,13 +33,14 @@ public class GlobalSuperstoreController {
   }
 
   @PostMapping("/submitItem")
-  public String handleSubmit(Item item) {
+  public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
     int index = getIndexFromId(item.getId());
     if (index == Constants.NOT_FOUND) {
       items.add(item);
     } else {
       items.set(index, item);
     }
+    redirectAttributes.addFlashAttribute("status", Constants.SUCCESS_STATUS);
     return "redirect:/inventory";
   }
 
