4e9a0f0529eb6401dd720330645db204fd0d0376
Katharine Lin <katharine.exe@gmail.com>
2023-09-26 12:41:30 +0200

    prevent updates if new date has 5+ day discrepancy

--- 02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
+++ 02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
@@ -3,6 +3,7 @@ package com.ltp.globalsuperstore;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -35,12 +36,15 @@ public class GlobalSuperstoreController {
   @PostMapping("/submitItem")
   public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
     int index = getIndexFromId(item.getId());
+    String status = Constants.SUCCESS_STATUS;
     if (index == Constants.NOT_FOUND) {
       items.add(item);
-    } else {
+    } else if (within5Days(item.getDate(), items.get(index).getDate())) {
       items.set(index, item);
+    } else {
+      status = Constants.FAILED_STATUS;
     }
-    redirectAttributes.addFlashAttribute("status", Constants.SUCCESS_STATUS);
+    redirectAttributes.addFlashAttribute("status", status);
     return "redirect:/inventory";
   }
 
@@ -53,4 +57,9 @@ public class GlobalSuperstoreController {
     }
     return Constants.NOT_FOUND;
   }
+
+  public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
+    }
 }
