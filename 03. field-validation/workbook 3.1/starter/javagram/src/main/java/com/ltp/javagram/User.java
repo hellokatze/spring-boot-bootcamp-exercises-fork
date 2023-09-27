616a7ccdc5defda56b74ea8e0fff3ef6922c58a8
Katharine Lin <katharine.exe@gmail.com>
2023-09-27 12:35:33 +0200

    add field validations to User model

--- 03. field-validation/workbook 3.1/starter/javagram/src/main/java/com/ltp/javagram/User.java
+++ 03. field-validation/workbook 3.1/starter/javagram/src/main/java/com/ltp/javagram/User.java
@@ -2,11 +2,24 @@ package com.ltp.javagram;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
+  @NotBlank(message = "First name can't be blank")
+  @Size(min = 2, message = "First name must be at least 2 characters")
   private String firstName;
+  @NotBlank(message = "Last name can't be blank")
+  @Size(min = 2, message = "Last name must be at least 2 characters")
   private String lastName;
+  @NotBlank(message = "Please choose a username")
+  @Size(min = 7, message = "Username must be at least 7 characters")
   private String username;
+  @Email(message = "Please enter a valid email address.")
   private String email;
+  @Past(message = "Date of birth must be in the past.")
   private Date dateOfBirth;
 
   public User(String firstName, String lastName, String username, String email, Date dateOfBirth) {
