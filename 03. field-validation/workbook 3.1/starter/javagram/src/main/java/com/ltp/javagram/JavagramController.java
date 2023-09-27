8610927286ab21ec3813bdae2d18177ffd43797c
Katharine Lin <katharine.exe@gmail.com>
2023-09-27 12:31:25 +0200

    add handleSubmit() method to receive POST request

--- 03. field-validation/workbook 3.1/starter/javagram/src/main/java/com/ltp/javagram/JavagramController.java
+++ 03. field-validation/workbook 3.1/starter/javagram/src/main/java/com/ltp/javagram/JavagramController.java
@@ -2,19 +2,26 @@ package com.ltp.javagram;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 
 @Controller
 public class JavagramController {
 
   @GetMapping("/")
   public String getForm(Model model) {
+    model.addAttribute("user", new User());
     return "sign-up";
   }
 
   @GetMapping("/result")
   public String getResult() {
     return "result";
+  }
+
+  @PostMapping("/submitForm")
+  public String handleSubmit(User user) {
+    return "redirect:/result";
   }
 }
